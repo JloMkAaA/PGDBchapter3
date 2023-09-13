3ee240949eab1c52b3ca1e1640a9937776130213
JloMkAaA <vladislav.tyazhelov5@gmail.com>
2023-09-13 12:56:39 +0300

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_page/home'
+  root 'static_page#home'
   get 'static_page/help'
   get 'static_page/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
