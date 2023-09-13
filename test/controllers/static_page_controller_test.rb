d152a0ac5c576d77d677977ea0cb0dfbfdd217b0
JloMkAaA <vladislav.tyazhelov5@gmail.com>
2023-09-13 12:19:27 +0300

    views

--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -4,10 +4,19 @@ class StaticPageControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_page_home_url
     assert_response :success
+    assert_select "title", "Home"
   end
 
   test "should get help" do
     get static_page_help_url
     assert_response :success
+    assert_select "title", "Help"
   end
+
+  test "should get about" do
+    get static_page_about_url
+    assert_response :success
+    assert_select "title", "About"
+  end
+  
 end
