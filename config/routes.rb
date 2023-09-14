725c0f22c21726eb606f9381158a9943f8e9bfba
JloMkAaA <vladislav.tyazhelov5@gmail.com>
2023-09-14 12:58:44 +0300

    User sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
   get 'users/new'
   root 'static_page#home'
   get 'help' => 'static_page#help'
   get 'about' => 'static_page#about'
   get 'signup' => 'users#new'
+  resources :users
+
 end
