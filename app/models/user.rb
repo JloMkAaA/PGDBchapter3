388b1e93b9cba2a9d826797f0628053066868533
JloMkAaA <vladislav.tyazhelov5@gmail.com>
2023-09-14 16:02:58 +0300

    Finish edit, update, index, destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     attr_accessor :remember_token
     before_save { self.username = username.downcase }
     validates :username, presence: true, length: {maximum: 50}, uniqueness: { case_sensitive: false }
-    validates :password, presence: true, length: {minimum: 6}
+    validates :password, presence: true, length: {minimum: 6}, allow_blank: true
 
     has_secure_password
     has_secure_password :recovery_password, validations: false
